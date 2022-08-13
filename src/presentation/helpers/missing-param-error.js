5c5bc0f97b15f13ca3e4e4e4ed79403b44de31fb
Rafael Maltez <rafaelmaltez89@gmail.com>
2022-08-13 16:27:14 -0300

    moves unathorized error to a helper method

--- src/presentation/helpers/missing-param-error.js
+++ src/presentation/helpers/missing-param-error.js
@@ -1,6 +1,6 @@
 module.exports = class MissingParamError extends Error {
   constructor (paramName) {
     super(`Missing param: ${paramName}`)
-    this.name = MissingParamError
+    this.name = 'MissingParamError'
   }
 }
