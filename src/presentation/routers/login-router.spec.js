5695610ca7f9b9361e52d4041bdce982ae2f95cb
Rafael Maltez <rafaelmaltez89@gmail.com>
2022-08-14 12:39:32 -0300

    ensures LoginRouter returns 500 if no AuthUseCase is provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -71,4 +71,15 @@ describe('Login router', () => {
     expect(httpResponse.statusCode).toBe(401)
     expect(httpResponse.body).toEqual(new UnauthorizedError())
   })
+  test('Should return 500 if no AuthUSeCase is provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+  })
 })
