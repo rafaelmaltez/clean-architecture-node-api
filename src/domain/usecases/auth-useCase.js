62bd1db74eae8cfdbd868ad0f568432ce37ad734
Rafael Maltez <rafaelmaltez89@gmail.com>
2022-08-22 21:30:55 -0300

    simplifies code in AuthUseCase auth method

--- src/domain/usecases/auth-useCase.js
+++ src/domain/usecases/auth-useCase.js
@@ -15,14 +15,11 @@ module.exports = class AuthUseCase {
       throw new MissingParamError('password')
     }
     const user = await this.loadUserByEmailRepository.load(email)
-    if (!user) {
-      return null
+    const isValid = user && await this.encrypter.compare(password, user.password)
+    if (isValid) {
+      const acessToken = await this.tokenGenerator.generate(user.id)
+      return acessToken
     }
-    const isValid = await this.encrypter.compare(password, user.password)
-    if (!isValid) {
-      return null
-    }
-    const acessToken = await this.tokenGenerator.generate(user.id)
-    return acessToken
+    return null
   }
 }
