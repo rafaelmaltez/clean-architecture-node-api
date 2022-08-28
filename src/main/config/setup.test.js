7b044a9a11ead2e819427ce1f01a150f24df9517
Rafael Maltez <rafaelmaltez89@gmail.com>
2022-08-28 13:57:15 -0300

    moves cors middleware test to its own file

--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -9,13 +9,4 @@ describe('App Setup', () => {
     const res = await request(app).get('/test-x-powered-by')
     expect(res.headers['x-powered-by']).toBeUndefined()
   })
-  test('Should enable CORS', async () => {
-    app.get('/test-cors', (req, res) => {
-      res.send('')
-    })
-    const res = await request(app).get('/test-cors')
-    expect(res.headers['access-control-allow-origin']).toBe('*')
-    expect(res.headers['access-control-allow-methods']).toBe('*')
-    expect(res.headers['access-control-allow-headers']).toBe('*')
-  })
 })
