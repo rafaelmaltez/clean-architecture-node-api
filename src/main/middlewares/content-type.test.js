316a80123e46376baa3478a37bc84d88a35f969d
Rafael Maltez <rafaelmaltez89@gmail.com>
2022-08-28 14:27:01 -0300

    ensures request return content-type xml if forced

--- src/main/middlewares/content-type.test.js
+++ src/main/middlewares/content-type.test.js
@@ -10,4 +10,13 @@ describe('Content-Type Middleware', () => {
       .get('/test-content-type')
       .expect('content-type', /json/)
   })
+  test('Should return xml content-type if forced', async () => {
+    app.get('/test-content-type-xml', (req, res) => {
+      res.type('xml')
+      res.send('')
+    })
+    await request(app)
+      .get('/test-content-type-xml')
+      .expect('content-type', /xml/)
+  })
 })
